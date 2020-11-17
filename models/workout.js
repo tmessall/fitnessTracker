ff759f02fd1ea8e4de84597f8152dee74a8c208c
tmessall <tmessall17@gmail.com>
2020-11-17 15:13:00 -0700

    Added some files and added the model and some api routes

---
+++ models/workout.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        weight: {
+            type: Number,
+            required: false
+        },
+        reps: {
+            type: Number,
+            required: false
+        },
+        sets: {
+            type: Number,
+            required: false
+        },
+        distance: {
+            type: Number,
+            required: false
+        }
+    }]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
---
+++ routes/api-routes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout.js");
+
+router.post("/api/workout", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/orkout", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
