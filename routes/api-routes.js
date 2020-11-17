fb4abbb580398772e5f468eb38b6b8bf4e047a7f
tmessall <tmessall17@gmail.com>
2020-11-17 15:52:54 -0700

    Fix for heroku

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
